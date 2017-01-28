8c5a20aaffba81684f0a8bf34da71fdf27f3efde
Ye, Xiuyi <xiye@microstrategy.com>
2017-01-28 18:53:02 -0500

    added service and routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,16 +3,29 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent}  from './app.component';
-import {HeroDetailComponent} from './hero-detial.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+import {HeroService} from './hero.service';
+
+import {AppRoutingModule} from './app-routing.module';
+
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule],
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
